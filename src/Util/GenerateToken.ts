cc346b50597d29e7ecbc99870f6095829d7f41d6
The One <officialtheselectedone@gmail.com>
2021-02-16 17:10:00 +0200

    added a comment

--- src/Util/GenerateToken.ts
+++ src/Util/GenerateToken.ts
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export const GenerateToken = (id: string) => {
+    //jwt.sign second parameter should be a secret token/password stored in .env file
     const token = jwt.sign(id, "string");
     return token;
 };
